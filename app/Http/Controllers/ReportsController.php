fc4e2e1c5902f18f0b9a2778c3aec35ddf12f678
pv <pv@ManuelCT>
2017-06-05 19:15:37 -0600

    Adding report for purchases projection

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use App\Product;
 
 class ReportsController extends Controller
@@ -29,11 +30,22 @@ class ReportsController extends Controller
   public function toBuyForm() {
        return view('reports.toBuyForm');
   }
-  public function toBuyRpt() {
+  public function toBuyRpt(Request $request) {
       /*Estimates products to be bought based on number of items actually
        * in storage, the daily average, and the next planned purchase date
+       * this report estimates the minimum number of items needed to be bougth
+       * in this purchase so when the next purchase is due there will be 
+       * a minimum number of products at the store
        */
+      $this_purchase_date = $request->get('this_purchase_date');
+      $next_purchase_date = $request->get('next_purchase_date');
+      $products_to_buy = $this->getProductsToBuy($this_purchase_date ,$next_purchase_date);
       
-       return view('reports.toBuyRpt');
+      return view('reports.toBuyRpt', compact('products_to_buy'));
+  }
+  
+  private function getProductsToBuy($this_purchase_date ,$next_purchase_date) {
+      $productsToBuy = 0;
+      return $productsToBuy;
   }
 }
